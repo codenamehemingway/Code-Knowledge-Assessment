72c4ac3680c44f62c151fd242b6e29a3f8b40cf0
codenamehemingway <jared.hemingway.jh@gmail.com>
2022-01-20 12:38:50 -0700

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,5 +25,95 @@ var saveScoreEl = document.getElementById("save-score-container");
 var highScoresList = document.getElementById("highScoreList");
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-// Starts game
-startBtn.addEventListener("click", startGame);
+highScoresList.innerHTML = highScores
+  .map((score) => {
+    return `<li class="high-score">${score.name}-${score.score}</li>`;
+  })
+  .join(" ");
+
+// Javascript variables
+// Questions courtesy of w3schools javascript quiz
+var secondsLeft;
+var questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: [
+      { choice: "<js>", correct: false },
+      { choice: "<javascript>", correct: false },
+      { choice: "<script>", correct: true },
+      { choice: "<scripted>", correct: false },
+    ],
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      { choice: "alertBox('Hello World');", correct: false },
+      { choice: "msgBox('Hello World');", correct: false },
+      { choice: "msg('Hello World');", correct: false },
+      { choice: "alert('Hello World');", correct: true },
+    ],
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    choices: [
+      { choice: "function myFunction()", correct: true },
+      { choice: "function = myFunction()", correct: false },
+      { choice: "function => (myFunction)", correct: false },
+      { choice: "function :: myFunction()", correct: false },
+    ],
+  },
+  {
+    question: "How do you call a function named 'myFunction'?",
+    choices: [
+      { choice: "call myFunction()", correct: false },
+      { choice: "myFunction()", correct: true },
+      { choice: "call function myFunction()", correct: false },
+      { choice: "function => function()", correct: false },
+    ],
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    choices: [
+      { choice: "if i = 5 then", correct: false },
+      { choice: "if (i == 5)", correct: true },
+      { choice: "if i = 5", correct: false },
+      { choice: "if i == 5 then", correct: false },
+    ],
+  },
+  {
+    question: "How does a FOR loop start?",
+    choices: [
+      { choice: "for i = 1 to 5", correct: false },
+      { choice: "for (i = 0; i < = 5)", correct: false },
+      { choice: "for (i <= 5; i++)", correct: false },
+      { choice: "for (i = 0; i <= 5; i++)", correct: true },
+    ],
+  },
+  {
+    question: "How can you add a comment in a JavaScript?",
+    choices: [
+      { choice: "'This is a comment'", correct: false },
+      { choice: "<!-- This is a comment -->", correct: false },
+      { choice: "<!== This is a comment ==>", correct: false },
+      { choice: "//This is a comment", correct: true },
+    ],
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    choices: [
+      { choice: "var colors = 'red','green','blue'", correct: false },
+      {
+        choice: " var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')",
+        correct: false,
+      },
+      {
+        choice: " var colors = (1:'red', 2:'green', 3:'blue')",
+        correct: false,
+      },
+      { choice: "var colors = ['red', 'green', 'blue']", correct: true },
+    ],
+  },
+];
+var points = 0;
+var maximumQuestions = questions.length;
+var currentQuestionIndex;
